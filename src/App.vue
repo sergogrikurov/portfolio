137d02137c100f72e5864e2dfc24421f086012d5
Sergo Grikurov <sergogrikurov@gmail.com>
2026-01-21 14:39:14 +0400

    start

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="header">Header</div>
+    <div class="main">
+      <RouterView />
+    </div>
+    <div class="footer">Footer</div>
+  </div>
+</template>
