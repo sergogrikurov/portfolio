3909b1fc0a5a5ffb1267a634518651acd8f4869d
Sergo Grikurov <sergogrikurov@gmail.com>
2026-01-23 16:30:29 +0400

    p2

--- src/router/index.js
+++ src/router/index.js
@@ -1,64 +1,28 @@
 import { createRouter, createWebHistory, RouterView } from "vue-router";
 import Home from "@/views/Home.vue";
-import Projects from "@/views/Projects.vue";
+import Works from "@/views/Works.vue";
 import About from "@/views/About.vue";
 import Contacts from "@/views/Contacts.vue";
 
 const routes = [
   {
-    path: "/",
-    redirect: "/en",
+    path: "/portfolio/",
+    redirect: "/portfolio/en",
   },
   {
-    path: "/:lang(en|ru)",
+    path: "/portfolio/:lang(en|ru)",
     component: RouterView,
     children: [
-      {
-        path: "",
-        name: "home",
-        component: Home,
-      },
-    ],
-  },
-  {
-    path: "/:lang(en|ru)",
-    component: RouterView,
-    children: [
-      {
-        path: "projects",
-        name: "projects",
-        component: Projects,
-      },
-    ],
-  },
-
-  {
-    path: "/:lang(en|ru)",
-    component: RouterView,
-    children: [
-      {
-        path: "about",
-        name: "about",
-        component: About,
-      },
-    ],
-  },
-
-  {
-    path: "/:lang(en|ru)",
-    component: RouterView,
-    children: [
-      {
-        path: "contacts",
-        name: "contacts",
-        component: Contacts,
-      },
+      { path: "", name: "home", component: Home },
+      { path: "works", name: "works", component: Works },
+      { path: "about", name: "about", component: About },
+      { path: "contacts", name: "contacts", component: Contacts },
     ],
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory("/portfolio/"),
   routes,
 });
 
